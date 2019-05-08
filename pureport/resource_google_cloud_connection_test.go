7c0e0b311a0c27d05b0ce094dfcb8ec2c2678129
kevinkirkup <kevin.kirkup@pureport.com>
2019-05-08 12:58:14 -0400

    Fix Acceptance Test check

--- pureport/resource_google_cloud_connection_test.go
+++ pureport/resource_google_cloud_connection_test.go
@@ -31,7 +31,7 @@ resource "google_compute_interconnect_attachment" "main" {
   name   = "terraform-acc-interconnect-${count.index + 1}"
   router = "${element(google_compute_router.main.*.self_link, count.index)}"
   type   = "PARTNER"
-	edge_availability_domain = "AVAILABILITY_DOMAIN_${count.index + 1}"
+  edge_availability_domain = "AVAILABILITY_DOMAIN_${count.index + 1}"
 
 
   count = 2
@@ -39,33 +39,33 @@ resource "google_compute_interconnect_attachment" "main" {
 
 
 data "pureport_accounts" "main" {
-	name_regex = "Terraform"
+  name_regex = "Terraform"
 }
 
 data "pureport_cloud_regions" "main" {
-	name_regex = "Los.*"
+  name_regex = "Los.*"
 }
 
 data "pureport_locations" "main" {
-	name_regex = "^Sea.*"
+  name_regex = "^Sea.*"
 }
 
 data "pureport_networks" "main" {
-	account_id = "${data.pureport_accounts.main.accounts.0.id}"
-	name_regex = "Bansh.*"
+  account_id = "${data.pureport_accounts.main.accounts.0.id}"
+  name_regex = "Bansh.*"
 }
 
 resource "pureport_google_cloud_connection" "main" {
-	name = "GoogleCloudTest"
-	speed = "50"
+  name = "GoogleCloudTest"
+  speed = "50"
 
-	location_href = "${data.pureport_locations.main.locations.0.href}"
-	network {
-		id = "${data.pureport_networks.main.networks.0.id}"
-		href = "${data.pureport_networks.main.networks.0.href}"
-	}
+  location_href = "${data.pureport_locations.main.locations.0.href}"
+  network {
+    id = "${data.pureport_networks.main.networks.0.id}"
+    href = "${data.pureport_networks.main.networks.0.href}"
+  }
 
-	primary_pairing_key = "${google_compute_interconnect_attachment.main.0.pairing_key}"
+  primary_pairing_key = "${google_compute_interconnect_attachment.main.0.pairing_key}"
 }
 `
 
