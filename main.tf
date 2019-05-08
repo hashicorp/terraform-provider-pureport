a912fa14b8e2541ddebc368ae04d9c320cf4e24e
kevinkirkup <kevin.kirkup@pureport.com>
2019-05-08 14:34:51 -0400

    Use Google to test main plugin execution

--- main.tf
+++ main.tf
@@ -1,13 +1,37 @@
+data "google_compute_network" "default" {
+  name = "default"
+}
+
+resource "google_compute_router" "main" {
+  name    = "terraform-acc-router-${count.index + 1}"
+  network = "${data.google_compute_network.default.name}"
+
+  bgp {
+    asn = "16550"
+  }
+
+  count = 2
+}
+
+resource "google_compute_interconnect_attachment" "main" {
+  name                     = "terraform-acc-interconnect-${count.index + 1}"
+  router                   = "${element(google_compute_router.main.*.self_link, count.index)}"
+  type                     = "PARTNER"
+  edge_availability_domain = "AVAILABILITY_DOMAIN_${count.index + 1}"
+
+  count = 2
+}
+
 data "pureport_accounts" "main" {
   name_regex = "Terraform"
 }
 
 data "pureport_cloud_regions" "main" {
-  name_regex = "Oregon"
+  name_regex = "Los.*"
 }
 
 data "pureport_locations" "main" {
-  name_regex = "^Ral*"
+  name_regex = "^Sea.*"
 }
 
 data "pureport_networks" "main" {
@@ -15,25 +39,16 @@ data "pureport_networks" "main" {
   name_regex = "Bansh.*"
 }
 
-resource "pureport_site_vpn_connection" "main" {
-  name              = "SiteVPNTest"
-  speed             = "100"
-  high_availability = true
+resource "pureport_google_cloud_connection" "main" {
+  name  = "GoogleCloudTest"
+  speed = "50"
 
-  location {
-    id   = "${data.pureport_locations.main.locations.0.id}"
-    href = "${data.pureport_locations.main.locations.0.href}"
-  }
+  location_href = "${data.pureport_locations.main.locations.0.href}"
 
   network {
     id   = "${data.pureport_networks.main.networks.0.id}"
     href = "${data.pureport_networks.main.networks.0.href}"
   }
 
-  ike_version  = "V2"
-  routing_type = "ROUTE_BASED_BGP"
-  customer_asn = 30000
-
-  primary_customer_router_ip   = "123.123.123.123"
-  secondary_customer_router_ip = "124.124.124.124"
+  primary_pairing_key = "${google_compute_interconnect_attachment.main.0.pairing_key}"
 }
